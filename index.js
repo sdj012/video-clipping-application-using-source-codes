f64b579f251bd02ca008d4844a62e8df67a2f700
sdj012 <49047379+sdj012@users.noreply.github.com>
2019-11-29 17:36:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var app = express();
 var variable=[];
 var videos=[];
 var playlist=[];
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT;
 var loggedInUser;
 // call this function after the http server starts listening for requests
 
