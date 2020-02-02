2d4df639f28a88fc05dac95e9fef6e2c4ee474d6
Salley Daeun Jeong <sdj@Salleys-MacBook-Pro.local>
2020-02-02 15:45:35 -0500

    ui update

--- my-app/home.js
+++ my-app/home.js
@@ -42,3 +42,58 @@
     return 0;
 
   }
+
+  function s(){
+
+   let videos=document.getElementsByClassName('video');
+
+   for(v of videos){
+     v.style.width="50%"
+     v.style.height="50%"
+   }
+
+   return 0;
+  }
+
+  function m(){
+
+    let videos=document.getElementsByClassName('video');
+ 
+    for(v of videos){
+      v.style.width="60%"
+      v.style.height="60%"
+    }
+ 
+    return 0;
+  }
+
+  function l(){
+
+    let videos=document.getElementsByClassName('video');
+ 
+    for(v of videos){
+      v.style.width="70%"
+      v.style.height="70%"
+    }
+ 
+    return 0;
+  }
+
+  function reveal(){
+
+    let field=document.getElementsByClassName('addInputBox');
+
+      for(f of field){
+
+        // if(f.style.visibility==="visible")f.style.visibility="hidden"
+        // else f.style.visibility="visible"
+
+        if(f.style.opacity==0)f.style.opacity=1
+        else f.style.opacity=0;
+        
+      }
+
+    return 0;
+
+  }
+
